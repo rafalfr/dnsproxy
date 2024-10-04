526ca72bbb19d56e20e48a33f1e118191e4316b2
Rafał Frączek <rafalfr@protonmail.ch>
2024-10-04 22:49:14 +0200

    update

--- main.go
+++ main.go
@@ -403,7 +403,7 @@ func runPprof(options *Options) {
 	go func() {
 		log.Info("pprof: listening on localhost:6060")
 		srv := &http.Server{
-			Addr:        "localhost:6060",
+			Addr:        "0.0.0.0:6060",
 			ReadTimeout: 60 * time.Second,
 			Handler:     mux,
 		}
