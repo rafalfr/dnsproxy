21f6bb2176d5c8029e4aed5e51bdc47c850d2efa
Rafał Frączek <rafalfr@protonmail.ch>
2024-11-26 17:57:26 +0100

    update

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+// https://stackoverflow.com/questions/74371890/post-context-deadline-exceeded-client-timeout-exceeded-while-awaiting-headers
+
 import (
 	"github.com/AdguardTeam/dnsproxy/internal/cmd"
 )
